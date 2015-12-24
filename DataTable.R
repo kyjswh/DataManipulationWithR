db833ff7495c6ed13ebd01ece1a6ece6146c1ca4
Daniel1002 <ZhengkanYang2014@u.northwestern.edu>
2015-12-24 15:51:13 -0800

    Update DataTable.R

--- DataTable.R
+++ DataTable.R
@@ -33,3 +33,49 @@ carriers <- unique(hflights_dt$UniqueCarrier)
 hflights_dt[,paste('carrier',carriers, sep='_') :=
                                         lapply(carriers, function(x) as.numeric(UniqueCarrier == x))]
 str(hflights_dt[,grep('^carrier',names(hflights_dt)),with=FALSE])
+
+#Merge Tables
+wdays <- data.table(
+    DayOfWeek = 1:7,
+    DayOfWeekString = c('Sunday',
+                        'Monday',
+                        'Tuesday',
+                        'Wednesday',
+                        'Thursday',
+                        'Friday',
+                        'Saturday')
+)
+
+
+system.time(merge(hflights, wdays))
+system.time(merge(hflights_dt, wdays, by='DayOfWeek'))
+  )
+
+
+#Reshape and Visualization
+#
+library(reshape2)
+melted_flights <- melt(hflights)
+
+melted_flights <- melt(hflights,
+                            id.vars = 0,
+                              measure.vars = c('ActualElapsedTime','AirTime'))
+
+str(melted_flights)
+head(melted_flights)
+
+#Boxplot
+library(ggplot2)
+ggplot(melted_flights,aes(x = variable, y = value)) + geom_boxplot()
+
+
+#long to wide format and aggregate
+melted_flights <- melt(hflights,
+                        id.vars = 'Month',
+                          measure.vars = c('ActualElapsedTime','AirTime'))
+df <- dcast(melted_flights, Month~variable, fun.aggregate=mean,na.rm=TRUE)
+
+ggplot(melt(df, id.vars = 'Month')) + 
+      geom_line(aes(x = Month, y = value, color = variable),size=1.2) + 
+        scale_x_continuous(breaks=1:12) + 
+          theme_bw() + theme(legend.position='top')
